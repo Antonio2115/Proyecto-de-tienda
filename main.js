767e357300ea91a807eda698df3f297b08fb7205
Jose Antonio VA <joseantoniova159@gmail.com>
2022-08-09 00:26:09 -0500

    Integración de aside de ordenes

--- main.js
+++ main.js
@@ -4,14 +4,30 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.icon_menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 
+const menuCarIcon  = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click',toggleDesktopMenu);
 menuHamIcon.addEventListener('click',toggleMobileMenu);
+menuCarIcon.addEventListener('click',toggleCarAside);
 
-function toggleDesktopMenu (){
+function toggleDesktopMenu() 
+{
+    mobileMenu.classList.add('inactive');
+    aside.classList.add('inactive');
     desktopMenu.classList.toggle('inactive');
 }
 
-function toggleMobileMenu (){
+function toggleMobileMenu()
+{
+    desktopMenu.classList.add('inactive');
+    aside.classList.add('inactive');
     mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCarAside()
+{
+    desktopMenu.classList.add('inactive');
+    mobileMenu.classList.add('inactive');
+    aside.classList.toggle('inactive');
 }
